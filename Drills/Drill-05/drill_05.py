5e7f9f02856b7aabf04ee83ea7e36b78e843817b
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-09-17 17:09:35 +0900

    drill-05 점과 점 사이 이동 구현
    이동시 방향과 프레임에 따른 스프라이트 출력 구현

--- Drills/Drill-05/drill_05.py
+++ Drills/Drill-05/drill_05.py
@@ -6,22 +6,25 @@ open_canvas()
 character = load_image('animation_sheet.png')
 
 
+
 def point_to_point(start_x, start_y, target_x, target_y):
+    frame = 0
     x, y = start_x, start_y
     vx, vy = (target_x - start_x)/20, (target_y - start_y)/20
     while x != target_x:
         clear_canvas()
+        frame = (frame + 1) % 8
         x += vx
         y += vy
         if vx > 0:
-            character.clip_draw(0 * 100, 100, 100, 100, x, y)
+            character.clip_draw(frame * 100, 100, 100, 100, x, y)
         else:
-            character.clip_draw(0 * 100, 0, 100, 100, x, y)
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
         update_canvas()
         delay(0.05)
 
 
-point_to_point(200,200,100,100)
+point_to_point(100,100,200,200)
 
 
 
