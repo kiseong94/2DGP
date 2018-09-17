fe77b6b4f9b6f4e4f499841df1e9b87adae8b297
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-09-17 17:03:43 +0900

    drill-05 점과 점 이동구현
    점과 점 이동구현

--- Drills/Drill-05/drill_05.py
+++ Drills/Drill-05/drill_05.py
@@ -6,5 +6,20 @@ open_canvas()
 character = load_image('animation_sheet.png')
 
 
+def point_to_point(start_x, start_y, target_x, target_y):
+    x, y = start_x, start_y
+    vx, vy = (target_x - start_x)/20, (target_y - start_y)/20
+    while x != target_x:
+        clear_canvas()
+        x += vx
+        y += vy
+        character.clip_draw(0 * 100, 100, 100, 100, x, y)
+        update_canvas()
+        delay(0.05)
+
+
+point_to_point(100,100,200,200)
+
+
 
 close_canvas()
