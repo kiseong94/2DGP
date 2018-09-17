4da4b9b228972f86aaabb4633f861fbabc184fe4
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-09-17 16:25:24 +0900

    drill-05 make_rectangle 내부함수 구현
    make_rectangle내부의 move_left 함수내용 구현

--- Drills/Drill-05/drill_05.py
+++ Drills/Drill-05/drill_05.py
@@ -12,7 +12,7 @@ def move_from_center_to_right():
         grass.draw_now(400, 30)
         character.draw_now(x, y)
         x += 5
-        delay(0.1)
+        delay(0.02)
 
 def move_up():
     x, y = 800 - 25, 90
@@ -21,10 +21,15 @@ def move_up():
         grass.draw_now(400, 30)
         character.draw_now(x, y)
         y += 5
-        delay(0.1)
-    pass
+        delay(0.02)
 def move_left():
-    pass
+    x, y = 800 - 25, 600 - 50
+    while x > 0 + 25:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x -= 5
+        delay(0.02)
 def move_down():
     pass
 def move_from_right_to_center():
@@ -32,7 +37,7 @@ def move_from_right_to_center():
 
 def make_rectangle():
     #move_from_center_to_right()
-    move_up()
+    #move_up()
     move_left()
     move_down()
     move_from_right_to_center()
