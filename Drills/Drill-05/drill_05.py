f3952994c01651b4eb673be8185c8cf4b296e957
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-09-17 17:26:31 +0900

    drill-05 반복동작
    좌표값 세트를 넣어 점과 점 이동을 반복 시킴

--- Drills/Drill-05/drill_05.py
+++ Drills/Drill-05/drill_05.py
@@ -5,13 +5,19 @@ open_canvas()
 
 character = load_image('animation_sheet.png')
 
+path = {(203,535),(132,243),(535,470) }
+path_index=0
+
+start_x, start_y = 203,535
 
 
 def point_to_point(start_x, start_y, target_x, target_y):
     frame = 0
+    cnt = 0
     x, y = start_x, start_y
     vx, vy = (target_x - start_x)/20, (target_y - start_y)/20
-    while x != target_x:
+
+    while cnt < 20:
         clear_canvas()
         frame = (frame + 1) % 8
         x += vx
@@ -21,11 +27,14 @@ def point_to_point(start_x, start_y, target_x, target_y):
         else:
             character.clip_draw(frame * 100, 0, 100, 100, x, y)
         update_canvas()
+        cnt+=1
         delay(0.05)
 
 
-point_to_point(100,100,200,200)
 
-
+while True:
+    for target_x,target_y in path:
+        point_to_point(start_x, start_y, target_x, target_y)
+        start_x, start_y= target_x, target_y
 
 close_canvas()
