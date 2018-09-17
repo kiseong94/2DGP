6225ba05884e70d1b7e2cadb0fb36eb495e98601
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-09-17 16:53:38 +0900

    drill-05 또 다른 시작
    입력된 경로를 따라 움직이는 캐릭터 구현하기

--- Drills/Drill-05/drill_05.py
+++ Drills/Drill-05/drill_05.py
@@ -3,71 +3,8 @@ import math
 
 open_canvas()
 
-grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
-def move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 5
-        delay(0.02)
 
-def move_up():
-    x, y = 800 - 25, 90
-    while y < 600 - 50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y += 5
-        delay(0.02)
-def move_left():
-    x, y = 800 - 25, 600 - 50
-    while x > 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x -= 5
-        delay(0.02)
-def move_down():
-    x, y = 25, 600 - 50
-    while y > 90:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y -= 5
-        delay(0.02)
-def move_from_right_to_center():
-    x, y = 25, 90
-    while x < 400:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 5
-        delay(0.02)
-
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_right_to_center()
-
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, 600 // 2 - 90
-    degree = -90
-    while degree < 270:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(cx + r * math.cos(math.radians(degree)),
-                           cy + r * math.sin(math.radians(degree)))
-        degree = degree + 5
-        delay(0.02)
-
-while True:
-    make_rectangle()
-    make_circle()
 
 close_canvas()
