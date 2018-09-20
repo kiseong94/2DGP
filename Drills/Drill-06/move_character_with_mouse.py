5e0b5493d8c0b192425eac0a9cfd7b1ad8dcc735
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-09-20 17:08:37 +0900

    drill_06 방향 추가
    direction 변수를 추가해서  방향에 맞는 이미지를 출력하도록 함

--- Drills/Drill-06/move_character_with_mouse.py
+++ Drills/Drill-06/move_character_with_mouse.py
@@ -1,12 +1,14 @@
 from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+left, right = -1, 1
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+direction = left
 
 def handle_events():
     global running, move
     global x, y
-    global cursor_x, cursor_y, vx, vy, t
+    global cursor_x, cursor_y, vx, vy, t, direction
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -19,6 +21,10 @@ def handle_events():
             vx = (target_x - x)/20
             vy = (target_y - y)/20
             t = 0
+            if vx < 0:
+                direction = left
+            else:
+                direction = right
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -27,14 +33,20 @@ def Move_To_Target():
     x = x + vx
     y = y + vy
     t += 1
-    character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
+    if direction == left:
+        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
 
     if t == 20:
         move = False
 
 
 def Stand_Still():
-    character.clip_draw(frame * 100, 100 * 2, 100, 100, x, y)
+    if direction == left:
+        character.clip_draw(frame * 100, 100 * 2, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 100 * 3, 100, 100, x, y)
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -59,7 +71,7 @@ while running:
     cursor.draw(cursor_x + cursor.w / 2, cursor_y - cursor.h / 2)
     frame = (frame + 1) % 8
     update_canvas()
-    delay(0.06)
+    delay(0.02)
     handle_events()
 
 close_canvas()
