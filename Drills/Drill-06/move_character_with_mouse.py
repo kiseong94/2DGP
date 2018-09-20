e80ee7e7dddd2070daf00fea7430d0ce9bf416c8
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-09-20 16:48:00 +0900

    drill_06 마우스 커서
    마우스 좌표에 마우스 이미지를 출력해서 커서 완성

--- Drills/Drill-06/move_character_with_mouse.py
+++ Drills/Drill-06/move_character_with_mouse.py
@@ -6,12 +6,13 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
+    global cursor_x, cursor_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            cursor_x, cursor_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -19,16 +20,20 @@ def handle_events():
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+cursor = load_image('hand_arrow.png')
+
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+cursor_x, cursor_y =  KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    frame = (frame + 1) % 8
+    cursor.draw(cursor_x + cursor.w / 2, cursor_y - cursor.h / 2)
+    update_canvas()
     delay(0.02)
     handle_events()
 
