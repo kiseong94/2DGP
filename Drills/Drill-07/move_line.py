a6d32b9b56b42c4a03cf583bab483d53a43825f3
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-10-01 17:01:47 +0900

    Drill_07 배경출력
    Drill_07 배경출력

--- Drills/Drill-07/move_line.py
+++ Drills/Drill-07/move_line.py
@@ -1,10 +1,12 @@
 from pico2d import *
 import random
 size=10
+n=1
 points = [(random.randint(-500,500),random.randint(-500,500)) for i in range(size)]
 
 
-open_canvas(1200, 800)
+open_canvas(1280, 1024)
+kpu_ground = load_image('KPU_GROUND.PNG')
 character = load_image('animation_sheet.png')
 
 
@@ -17,6 +19,12 @@ def draw_line(p1, p2):
        y = (1-t)*p1[1]+t*p2[1]
 
 while True:
+    clear_canvas()
+    kpu_ground.draw(1280//2, 1024//2)
     draw_line(points[n-1], points[n])
+    update_canvas()
     n = (n+1) % size
+
+    delay(0.02)
+
 
