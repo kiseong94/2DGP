29402d2dd4bfd3fed0c8f6d55f5c20d63f134a92
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-10-01 17:06:01 +0900

    Drill_07 캐릭터 이동구현
    캐릭터 선형이동

--- Drills/Drill-07/move_line.py
+++ Drills/Drill-07/move_line.py
@@ -2,7 +2,9 @@ from pico2d import *
 import random
 size=10
 n=1
-points = [(random.randint(-500,500),random.randint(-500,500)) for i in range(size)]
+prev_x=0
+frame=0
+points = [(random.randint(0,1280),random.randint(0,1024)) for i in range(size)]
 
 
 open_canvas(1280, 1024)
@@ -10,21 +12,23 @@ kpu_ground = load_image('KPU_GROUND.PNG')
 character = load_image('animation_sheet.png')
 
 
-def draw_line(p1, p2):
+def draw_character(p1, p2):
 
 
    for i in range(0, 100 + 1, 2):
+       clear_canvas()
+       kpu_ground.draw(1280 // 2, 1024 // 2)
        t = i/100
        x = (1-t)*p1[0]+t*p2[0]
        y = (1-t)*p1[1]+t*p2[1]
+       character.clip_draw(100, 100 * 2, 100, 100, x, y)
+       update_canvas()
+       delay(0.06)
+
 
 while True:
-    clear_canvas()
-    kpu_ground.draw(1280//2, 1024//2)
-    draw_line(points[n-1], points[n])
-    update_canvas()
+    draw_character(points[n-1], points[n])
     n = (n+1) % size
 
-    delay(0.02)
 
 
