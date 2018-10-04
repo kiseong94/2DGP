92f7b53f135be219797d7e7aa63668c21f5da732
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-10-04 16:51:06 +0900

    drill_08_2 캐릭터 방향, 움직임 구현
    캐릭터가 방향에 맞게 움직이도록 구현

--- Drills/Drill-08/drill_08_2.py
+++ Drills/Drill-08/drill_08_2.py
@@ -6,6 +6,9 @@ points = [(100, 100),(300,350),(200,500),(100,200)]
 n=0
 size=4
 
+frame = 0
+prev_x = 0
+
 KPU_WIDTH, KPU_HEIGHT = 1280, 900
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 character = load_image('animation_sheet.png')
@@ -13,8 +16,8 @@ kpu_ground = load_image('KPU_GROUND.png')
 
 
 def draw_curve_4_points(p1, p2, p3, p4):
-
-
+    global frame
+    global prev_x
 
     for i in range(0, 100, 2):
         clear_canvas()
@@ -22,9 +25,14 @@ def draw_curve_4_points(p1, p2, p3, p4):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
         y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        character.clip_draw(100, 100 * 3, 100, 100, x, y)
+        if x > prev_x:
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        else:
+            character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
+        frame=(frame+1)%8
+        prev_x=x
         update_canvas()
-        delay(0.06)
+        delay(0.02)
 
 
 
