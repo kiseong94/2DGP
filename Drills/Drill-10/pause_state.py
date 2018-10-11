a3cd309d3652267fb83f7d492058b7daa366784e
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-10-11 18:20:17 +0900

    Drill_10_1 pause_state.py 추가
    pause_state.py 추가

---
+++ Drills/Drill-10/pause_state.py
@@ -0,0 +1,48 @@
+import game_framework
+import main_state
+from pico2d import *
+
+
+name = "PauseState"
+image = None
+
+
+def enter():
+    global image
+
+
+
+def exit():
+    global image
+
+
+
+def handle_events():
+    pass
+
+
+def draw():
+    pass
+
+
+
+
+
+
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+
+
+
+
