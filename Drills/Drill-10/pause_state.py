f00e8d46c4ed99e0529d79d15be737e9eabd07d6
kiseong94 <43131720+kiseong94@users.noreply.github.com>
2018-10-11 18:24:10 +0900

    Drill_10_1 pause 이미지 추가
    pause 이미지 추가
    draw 함수, event 함수 내부 구현

--- Drills/Drill-10/pause_state.py
+++ Drills/Drill-10/pause_state.py
@@ -9,20 +9,29 @@ image = None
 
 def enter():
     global image
-
+    image = load_image('pause_image.png')
 
 
 def exit():
     global image
-
+    del(image)
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+                game_framework.pop_state()
+
 
 
 def draw():
-    pass
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
 
 
 
